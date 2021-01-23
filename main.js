9babc15038ab02d0f6acf8e5672a4b3b1bb57c04
abu-samRah <abdallahabuamrah@gmail.com>
2021-01-23 13:24:08 +0300

    upload NodeJS Task

---
+++ main.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const csv = require('csv-parser');
+
+let readForm = "MOCK_DATA.csv" // the file that the data will be read from.
+let outputFile = "output.json" // the file that the data will be written to.
+
+/*
+Desc: This function is responsbile for reading the data from the readFrom file, parse it, 
+save it to an array, and then calls the saveToFile function.
+Args: readFrom=> contains the file name to read the data from.
+*/
+function readPlain(readForm) {
+    let users = []
+        /* 
+        Here, we create a readStream using the fs module, pipe it into the csv object that will then 
+        fire the data event each time a new row from the CSV file is processed. The end event is triggered 
+        when all the rows from the CSV file are processed and we log a short message to the console to indicate that.
+        */
+    fs.createReadStream(readForm)
+        .pipe(csv())
+        .on('data', (row) => {
+            users.push(row);
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+            saveToFile(users, outputFile)
+        });
+}
+
+/*
+Desc: This function is responsbile for creating an output file with the name passed 
+as outputFile argument, then saving the passed array data to the created file. 
+Args: dataToSave=> an array containig the users data to save, outputFile=> contains the file name to create.
+*/
+function saveToFile(dataToSave, outputFile) {
+    let file = fs.createWriteStream(outputFile);
+    file.on('error', function(err) { console.log(err) });
+    file.write('[' + '\n') // addding the beginning of the json file
+
+    // loops over the array objects and writes them to the file 
+    dataToSave.forEach(function(v, idx, array) {
+        if (idx === array.length - 1) {
+            file.write(JSON.stringify(v) + '\n');
+        } else {
+            file.write(JSON.stringify(v) + ',' + '\n');
+        }
+    });
+    file.write(']') // addding the ending of the json file
+    file.end();
+    file.on("finish", () => {
+        console.log('JSON file successfully created and populated with the passed data');
+        readJsonFile()
+    }); //calling the readJsonFile function when finishing populating the file with the objects
+}
+
+/*
+Desc: This function is responsbile for reading the crerated file and logs all the parsed objects. 
+Args: None
+*/
+function readJsonFile() {
+    fs.readFile('output.json', 'utf8', (err, dataRow) => {
+        if (err) throw err
+        let user = JSON.parse(dataRow)
+        console.log(user);
+    });
+}
+
+readPlain(readForm)
